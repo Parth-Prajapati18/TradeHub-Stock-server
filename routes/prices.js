7c57d35fe1acd3eaf61622a35b4b27544296f759
Parthkumar <parthb1806@gmail.com>
2024-04-09 18:19:14 -0400

    modified the prices socket

--- routes/prices.js
+++ routes/prices.js
@@ -4,11 +4,24 @@ let currentPrices = null;
 
 const fetchPrice = async () => {
     try {
-        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,cardano,solana,ripple,binancecoin,polkadot,dogecoin,litecoin,wrapped-bitcoin,chainlink,uniswap,polygon,internet-computer,near-protocol,litecoin,aptos,leo-token,ethereum-classic,stacks,filecoin,mantle,cosmos,arbitrum,immutable,render-token,bittensor,first-digital-usd,eos&vs_currencies=usd');
-        currentPrices = response.data;
-        console.log(`Updated Prices: ${JSON.stringify(currentPrices)} as of ${new Date()}`);
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,cardano,solana,ripple,binancecoin,polkadot,dogecoin,litecoin,wrapped-bitcoin,chainlink,uniswap,polygon,internet-computer,near-protocol,aptos,leo-token,ethereum-classic,stacks,filecoin,mantle,cosmos,arbitrum,immutable,render-token,bittensor,first-digital-usd,eos&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h');
+        currentPrices = response.data.map(crypto => ({
+            id: crypto.id,
+            symbol: crypto.symbol,
+            name: crypto.name,
+            image: crypto.image,
+            current_price: crypto.current_price,
+            market_cap: crypto.market_cap,
+            market_cap_rank: crypto.market_cap_rank,
+            total_volume: crypto.total_volume,
+            high_24h: crypto.high_24h,
+            low_24h: crypto.low_24h,
+            price_change_24h: crypto.price_change_24h,
+            price_change_percentage_24h: crypto.price_change_percentage_24h,
+            last_updated: crypto.last_updated
+        }));
     } catch (error) {
-        console.error(`Error fetching Bitcoin price: ${error}`);
+        console.error(`Error fetching cryptocurrency prices: ${error}`);
     }
 };
 
@@ -22,12 +35,11 @@ module.exports = (io) => {
         if (currentPrices !== null) {
             socket.emit('priceUpdate', currentPrices);
         }
-
         const priceUpdateInterval = setInterval(() => {
             if (currentPrices !== null) {
                 socket.emit('priceUpdate', currentPrices);
             }
-        }, 1000);
+        }, 30000);
 
         socket.on('disconnect', () => {
             console.log('Client disconnected from /prices');
